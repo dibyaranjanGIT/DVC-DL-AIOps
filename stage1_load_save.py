873dc06580025480529aa925a0b1de6415caf44b
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2022-01-10 12:22:51 +0530

    Adding changes to stage1_load_save

--- stage1_load_save.py
+++ stage1_load_save.py
@@ -5,20 +5,12 @@ import os
 
 def get_data(config_path):
     config = read_config(config_path)
-    remote_data_path = config["data_source"]
-    df = pd.read_csv(remote_data_path, sep=";")
 
-    # Save data set in local
-    artifcats_dir = config["artifacts"]["artifcats_dir"]
-    raw_file_dir = config["artifacts"]["raw_local_dir"]
-    raw_local_file = config["artifacts"]["raw_local_file"]
+    source_data_dir = config["source_data_paths"]
+    local_data_dir = config["data_paths"]
 
-    raw_local_dir_path = os.path.join(artifcats_dir, raw_file_dir)
-    create_directory(dirs=[raw_local_dir_path])
-
-    raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
-    df.to_csv(raw_local_file_path, sep=";", index=False)
-
+    for source_data, local_data in zip(source_data_dir, local_data_dir):
+        create_directory(local_data)
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configuration/config.yaml")
